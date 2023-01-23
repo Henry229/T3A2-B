381f8fb596a902b35cedb0922b3199c466ba1420
lmh4686 <lmh4686@gmail.com>
2023-01-23 21:32:09 +1100

    Working on Header CSS

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import { GrRestaurant } from 'react-icons/gr';
 import { Link } from 'react-router-dom';
+import './header.css'
 
 const Header = () => {
   return (
-    <nav>
-      <Link to='/'>
-        <GrRestaurant />
-      </Link>
-      <ul>
-        <li>
-          <Link to='/menu'>Menu</Link>
-        </li>
-        <li>
-          <Link to='/reservation'>Reservation</Link>
-        </li>
-        <li>
-          <Link to='/aboutUs'>About Us</Link>
-        </li>
-        <li>
-          <Link to='/admin'>Admin</Link>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <Link to='/'><p>The Gangnam Style BBQ</p></Link>
+      <nav>
+        <ul>
+          <li><Link to='/menu'>Menu</Link></li>
+          <li><Link to='/reservation'>Reservation</Link></li>
+          <li><Link to='/aboutUs'>About Us</Link></li>
+          <li><Link to='/admin'>Admin</Link></li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
