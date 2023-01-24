9489f914b70798fb8b8fea9593aef3187de5b5f2
lmh4686 <lmh4686@gmail.com>
2023-01-24 21:57:08 +1100

    switch for updateFetch

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,17 +5,16 @@ const Footer = () => {
   return (
     <footer>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-      <section id='icons'>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      <section id='footerIcons'>
         <a href="#" class="fa fa-instagram fa-2x"></a>
         <a href="#" class="fa fa-facebook fa-2x"></a>
-        <a href="#" class="fa fa-twitter fa-2x"></a>
-        <a href="#" class="fa fa-whatsapp fa-2x"></a>
+        <button class="material-symbols-outlined">mail</button>
+        <button class="material-symbols-outlined">call</button>
       </section>
-      <section id='email'>
-        mashita@gmail.com
-      </section>
-      <section id='ph'>
-        012-345-678
+      <section id='footerText'>
+          <p>mashita@gmail.com</p>
+          <p>012-345-678</p>
       </section>
     </footer>
   );
