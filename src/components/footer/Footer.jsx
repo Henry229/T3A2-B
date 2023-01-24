fe0f47ad65213ca4c45921c36ba094a56262cac2
lmh4686 <lmh4686@gmail.com>
2023-01-24 20:02:26 +1100

    Footer done Header miner change

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './footer.css'
 
 const Footer = () => {
   return (
     <footer>
-      <h2>Footer</h2>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+      <section id='icons'>
+        <a href="#" class="fa fa-instagram fa-2x"></a>
+        <a href="#" class="fa fa-facebook fa-2x"></a>
+        <a href="#" class="fa fa-twitter fa-2x"></a>
+        <a href="#" class="fa fa-whatsapp fa-2x"></a>
+      </section>
+      <section id='email'>
+        mashita@gmail.com
+      </section>
+      <section id='ph'>
+        012-345-678
+      </section>
     </footer>
   );
 };
