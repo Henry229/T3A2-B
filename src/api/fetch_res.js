13ba88f69d60ac76fba1fc3bcec0b718fbf24fdb
Henry Chun <chunhyungjoon@gmail.com>
2023-02-03 09:06:01 +1100

    switch api address to https://mern-restaurant-api-production.up.railway.app/ in fetch_res.js

--- src/api/fetch_res.js
+++ src/api/fetch_res.js
@@ -21,10 +21,13 @@ export async function adminLogin(loginInfo) {
     const { id, password } = loginInfo;
     const headers = new Headers();
     headers.set('Authorization', 'Basic ' + base64.encode(id + ':' + password));
-    const response = await fetch('http://localhost:3000/admin/login', {
-      method: 'POST',
-      headers: headers,
-    });
+    const response = await fetch(
+      'https://mern-restaurant-api-production.up.railway.app/admin/login',
+      {
+        method: 'POST',
+        headers: headers,
+      }
+    );
     if (response.ok) {
       const data = await response.json();
       return data;
@@ -46,10 +49,13 @@ export async function getAllClient(jwt) {
   try {
     const headers = new Headers();
     headers.set('jwt', jwt);
-    const response = await fetch('http://localhost:3000/reservation', {
-      method: 'GET',
-      headers: headers,
-    });
+    const response = await fetch(
+      'https://mern-restaurant-api-production.up.railway.app/reservation',
+      {
+        method: 'GET',
+        headers: headers,
+      }
+    );
     if (response.ok) {
       const data = await response.json();
       console.log(data);
@@ -74,7 +80,7 @@ export async function updateClient(jwt, body, sendId) {
     headers.set('jwt', jwt);
     headers.append('Content-Type', 'application/json');
     const response = await fetch(
-      `http://localhost:3000/reservation/${sendId}`,
+      `https://mern-restaurant-api-production.up.railway.app/reservation/${sendId}`,
       {
         method: 'PUT',
         headers: headers,
@@ -107,7 +113,7 @@ export async function deleteClient(jwt, deleteId) {
     headers.set('jwt', jwt);
     headers.append('Content-Type', 'application/json');
     const response = await fetch(
-      `http://localhost:3000/reservation/${deleteId}`,
+      `https://mern-restaurant-api-production.up.railway.app/reservation/${deleteId}`,
       {
         method: 'DELETE',
         headers: headers,
@@ -138,12 +144,15 @@ export async function searchMobile(jwt, mobile) {
   const headers = new Headers();
   headers.set('jwt', jwt);
   headers.append('Content-Type', 'application/json');
-  const response = await fetch(`http://localhost:3000/reservation/${mobile}`, {
-    method: 'GET',
-    headers: headers,
-    // body: body,
-    redirect: 'follow',
-  });
+  const response = await fetch(
+    `https://mern-restaurant-api-production.up.railway.app/reservation/${mobile}`,
+    {
+      method: 'GET',
+      headers: headers,
+      // body: body,
+      redirect: 'follow',
+    }
+  );
   const data = await response.json();
   return data;
 }
